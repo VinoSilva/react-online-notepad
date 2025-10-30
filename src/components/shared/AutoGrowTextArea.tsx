f09e1084c250db8539c0e14cbd2603451f30171d
Vino <VinoshenSilva@gmail.com>
2025-10-30 15:30:03 +0800

    Fix text area not growing

--- src/components/shared/AutoGrowTextArea.tsx
+++ src/components/shared/AutoGrowTextArea.tsx
@@ -1,24 +1,27 @@
 // Import libraries
-import { useRef, useEffect } from "react";
-
+import { useEffect, useState } from "react";
 const AutoGrowTextarea = ({
   value,
   onChange,
   ...props
 }: React.TextareaHTMLAttributes<HTMLTextAreaElement>) => {
-  const ref = useRef<HTMLTextAreaElement>(null);
+  const [ref, setRef] = useState<HTMLTextAreaElement>();
 
   useEffect(() => {
-    const el = ref.current;
+    const el = ref;
     if (!el) return;
     el.style.height = "auto"; // reset to shrink if needed
     el.style.height = `${el.scrollHeight}px`; // grow to fit content
-  }, [value]); // runs whenever text changes
+  }, [value, ref]); // runs whenever text changes
 
   return (
     <textarea
       {...props}
-      ref={ref}
+      ref={(ref) => {
+        if (ref) {
+          setRef(ref);
+        }
+      }}
       value={value}
       onChange={onChange}
       className={`resize-none overflow-hidden ${props.className ?? ""}`}
